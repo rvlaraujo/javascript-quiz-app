ad07a7e5cd42b106232389bb97df81d0ec1bd532
Rafael Viana <rvlaraujo@gmail.com>
2021-12-11 13:21:48 -0300

    Creating a progress bar for the answers

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -54,7 +55,10 @@ getNewQuestion = () => {
   }
 
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -78,7 +82,7 @@ choices.forEach((choice) => {
 
     const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-    if(classToApply === 'correct') {
+    if (classToApply === 'correct') {
       incrementScore(CORRECT_BONUS);
     }
 
